899932e8e2315c48aa830c6c6e1a59d5ef3fd75e
Jakub Pałka <kubpal@gmail.com>
2022-08-19 19:53:11 +0200

    API code refactor

--- tests/API1.spec.js
+++ tests/API1.spec.js
@@ -1,46 +1,23 @@
-const {test, expect, response, request} = require('@playwright/test');
+const {test, expect, request} = require('@playwright/test');
+const { APIUtils } = require('./utils/APIUtils');
 const loginPayload = {userEmail: "lana@del.rey", userPassword: "Testy1!2@"};
 const orderPayload = {orders:[{country:"Cuba",productOrderedId:"6262e95ae26b7e1a10e89bf0"}]};
-let orderId;
 
-// test("First PW test", async ({browser,page})=>
-// {
-//     // const context = await browser.newContext();
-//     // const page = await context.newPage();
-//     await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
-// });
-let token;
+let response;
 test.beforeAll( async () =>
 {
     const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-    {
-        data:loginPayload
-    });
-    expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson = await loginResponse.json();
-    token = loginResponseJson.token;
-    console.log(token);
-
+    const apiUtils = new APIUtils(apiContext, loginPayload);
+    response = await apiUtils.createOrder(orderPayload);
     //order
-    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-    {
-       data: orderPayload,
-       headers:{
-            'Authorization': token,
-            'Content-Type': 'application/json'
-       },  
-    })
-    const orderResponseJson = await orderResponse.json();
-    console.log(orderResponseJson);
-    orderId = orderResponseJson.orders[0];
-    });
+
+});
 
 test.only("place order by API", async ({page})=>
 {
     await page.addInitScript(value => {
         window.localStorage.setItem('token', value)
-    }, token);
+    }, response.token);
     
     await page.goto("https://rahulshettyacademy.com/client");
    
@@ -52,7 +29,7 @@ test.only("place order by API", async ({page})=>
     // await page.pause();
     for(let i=0; i<tableCount; i++){
         const id = await table.locator("tbody th").nth(i).textContent();
-        if(orderId.includes(id)){
+        if(response.orderId.includes(id)){
             await table.locator("button").nth(2*i).click();
             break;
         }
